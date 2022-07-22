2ab78e9894f75f2db9d2496b31f6b41da85e2d1d
tanquoi <81850903+tanquoi@users.noreply.github.com>
2022-07-22 14:51:20 +0700

    Enemy

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 4;
+
 
     ScoreBoard scoreBoard;
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        Addrigidbody();
     }
 
-    // Start is called before the first frame update
+    private void Addrigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     public void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if(hitPoints < 1)
+        {
+           KillEnemy();
+        }    
+        
+    }
+    public void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
     }
     public void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
-    }
-    public void ProcessHit()
-    {
-        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
